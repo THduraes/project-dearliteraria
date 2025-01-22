5d08e4e75c0f4b5224e037c2bf19995e04183689
ThiagoDuraes <thiagosantos881@hotmail.com>
2025-01-21 23:49:52 -0300

    propeties, book, h2 orm

--- src/main/java/com/dearliteraria/first_spring_app/entities/Books.java
+++ src/main/java/com/dearliteraria/first_spring_app/entities/Books.java
@@ -1,4 +1,23 @@
 package com.dearliteraria.first_spring_app.entities;
 
-public class Books {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+    @Getter
+    @Setter
+
+    @Entity
+    @Table(name = "tb_book")
+    public class Books {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String title;
+        private String author;
+        private String genre;
+        private String imgUrl;
+        @Column(name = "book_year")
+        private Integer year;
+        private String descripition;
 }
