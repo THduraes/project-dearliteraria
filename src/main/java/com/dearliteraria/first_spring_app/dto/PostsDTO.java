be7d1428e0bb264e57c8616f2cdcf76bbd4fb0a8
ThiagoDuraes <thiagosantos881@hotmail.com>
2025-01-23 20:51:37 -0300

    Data updt

---
+++ src/main/java/com/dearliteraria/first_spring_app/dto/PostsDTO.java
@@ -0,0 +1,2 @@
+package com.dearliteraria.first_spring_app.dto;public class PostsDTO {
+}
---
+++ src/main/java/com/dearliteraria/first_spring_app/repositories/PostsRepository.java
@@ -0,0 +1,2 @@
+package com.dearliteraria.first_spring_app.repositories;public interface PostsRepository {
+}
